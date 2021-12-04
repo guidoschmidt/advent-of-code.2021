c0fcf7422a8edf70c3c55fba8ce044b074b07b6d
Guido Schmidt <git@guidoschmidt.cc>
2021-12-04 23:34:47 +0100

    ADD/day3.part3

--- day3/day3.js
+++ day3/day3.js
@@ -21,3 +21,90 @@ const epsilonRate10 = parseInt(epsilonRate2, 2);
 console.log("Gamma Rate: ", gammaRate2, gammaRate10);
 console.log("Epsilon Rate: ", epsilonRate2, gammaRate10);
 console.log("Power consumption: ", gammaRate10 * epsilonRate10);
+console.log("-------------------");
+
+function lifeSupportCalculatorFn(indexCount, startArray, testFn) {
+  return new Array(indexCount).fill(0).reduce(
+    (obj, el) => {
+      if (obj.bitArray.length === 1) {
+        return obj;
+      }
+      const c0 = obj.bitArray.reduce(
+        (sum, bitArray) => (bitArray[obj.index] === 0 ? sum + 1 : sum),
+        0
+      );
+      const c1 = obj.bitArray.reduce(
+        (sum, bitArray) => (bitArray[obj.index] === 1 ? sum + 1 : sum),
+        0
+      );
+      return testFn({ c0, c1, obj });
+    },
+    { bitArray: startArray, index: 0 }
+  );
+}
+
+const oxygenCo2Input = input.map((el) => el.split("").map((s) => parseInt(s)));
+
+const oxygenGeneratorRatingResult = lifeSupportCalculatorFn(
+  result.length,
+  oxygenCo2Input,
+  ({ c0, c1, obj }) => {
+    if (c0 > c1) {
+      return {
+        bitArray: obj.bitArray.filter((el) => el[obj.index] === 0),
+        index: obj.index + 1,
+      };
+    }
+    if (c1 > c0) {
+      return {
+        bitArray: obj.bitArray.filter((el) => el[obj.index] === 1),
+        index: obj.index + 1,
+      };
+    }
+    if (c1 === c0) {
+      return {
+        bitArray: obj.bitArray.filter((el) => el[obj.index] === 1),
+        index: obj.index + 1,
+      };
+    }
+    return { bitArray: obj.bitArray, index: obj.index + 1 };
+  }
+);
+
+const co2ScrubberRatingResult = lifeSupportCalculatorFn(
+  result.length,
+  oxygenCo2Input,
+  ({ c0, c1, obj }) => {
+    if (c0 < c1) {
+      return {
+        bitArray: obj.bitArray.filter((el) => el[obj.index] === 0),
+        index: obj.index + 1,
+      };
+    }
+    if (c1 < c0) {
+      return {
+        bitArray: obj.bitArray.filter((el) => el[obj.index] === 1),
+        index: obj.index + 1,
+      };
+    }
+    if (c1 === c0) {
+      return {
+        bitArray: obj.bitArray.filter((el) => el[obj.index] === 0),
+        index: obj.index + 1,
+      };
+    }
+    return { bitArray: obj.bitArray, index: obj.index + 1 };
+  }
+);
+const oxygenGeneratorRating = parseInt(
+  oxygenGeneratorRatingResult.bitArray.pop().join(""),
+  2
+);
+const co2ScrubberRating = parseInt(
+  co2ScrubberRatingResult.bitArray.pop().join(""),
+  2
+);
+console.log("Oxygen Generator Rating: ", oxygenGeneratorRating);
+console.log("CO² Scrubber Rating: ", co2ScrubberRating);
+const lifeSupport = oxygenGeneratorRating * co2ScrubberRating;
+console.log("Life Support", lifeSupport);
